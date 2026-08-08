bffbe78ba92997c8e870ff3ffd85a8cc76cbb57b
skerishKang <mtmsylove@gmail.com>
2026-08-08 21:16:07 +0900

    feat(v2-a): export visual fidelity module

---
+++ 04_개발/frontend/src/v2/visual/index.ts
@@ -0,0 +1,11 @@
+import '../v2-visual.css';
+
+export * from './visual-data';
+export * from './V2BenefitVisual';
+export * from './V2CinematicScenes';
+export * from './V2ExplorerPrimitives';
+export * from './V2Hero';
+export * from './V2Icon';
+export * from './V2Topbar';
+export * from './V2VisualFrame';
+export * from './V2VisualImage';
