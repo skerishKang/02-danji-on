372c9995d704a414bd06c46127362fb429894c19
skerishKang <mtmsylove@gmail.com>
2026-08-08 14:49:11 +0900

    feat(demo): mark rehearsal complete on ending

--- 04_개발/frontend/src/ending.tsx
+++ 04_개발/frontend/src/ending.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { installDemoServiceWorker } from './demo-service-worker';
+import { installDemoSessionTracking, markDemoComplete, readDemoSession } from './demo-state';
 import EndingPage from './EndingPage';
 import './ending.css';
 
@@ -8,3 +10,7 @@ createRoot(document.getElementById('ending-root')!).render(
     <EndingPage />
   </StrictMode>
 );
+
+installDemoSessionTracking('생활경제 엔딩');
+void installDemoServiceWorker();
+if (readDemoSession().status === 'running') markDemoComplete();
